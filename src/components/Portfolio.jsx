856a186065548e066f5982965adde0cd3228aaea
Leah Tillman <leahtillman@Leahs-MacBook-Air.local>
2023-12-11 09:28:39 -0600

    tested deplayment, fixed some minor styling things

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,37 +9,37 @@ import notePad from "../../images/notepad.png"
 export default function UserPage() {
     const projectData = [
         {
-            title: "• Fact Quiz •",
+            title: "Fact Quiz",
             url: "https://lrltillman.github.io/fact-quiz/",
             img: factQuiz,
             repo: "https://github.com/lrltillman/fact-quiz"
         },
         {
-            title: "• Password Generator •",
+            title: "Password Generator",
             url: "https://lrltillman.github.io/password-generator/",
             img: passwordGen,
             repo: "https://github.com/lrltillman/password-generator"
         },
         {
-            title: "• Weather Dashboard •",
+            title: "Weather Dashboard",
             url: "https://lrltillman.github.io/weather-dash/",
             img: weatherDash,
             repo: "https://github.com/lrltillman/weather-dash"
         },
         {
-            title: "• Notepad •",
+            title: "Notepad",
             url: "https://pwa-homework-6e5c6f37c6be.herokuapp.com/",
             img: pwa,
             repo: "https://github.com/lrltillman/pwa-assignment"
         },
         {
-            title: "• Day Planner •",
+            title: "Day Planner",
             url: "https://lrltillman.github.io/day-planner/",
             img: dayPlanner,
             repo: "https://github.com/lrltillman/day-planner"
         },
         {
-            title: "• Note Taker •",
+            title: "Note Taker",
             url: "https://note-taker-lrltillman-5594b15e77c1.herokuapp.com/",
             img: notePad,
             repo: "https://github.com/lrltillman/note-taker"
@@ -57,9 +57,9 @@ export default function UserPage() {
                         <div>
                             <img className="port-img" src={project.img}
                                 alt={project.title} />
-                            <div className="sassy-container">
-                                <a className="link-success link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href={project.url} target="_blank">Deployed application</a>
-                                <a className="link-success link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href={project.repo} target="_blank">Github repository</a>
+                            <div className="somewhat-sassy">
+                                <a className="link-success link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href={project.url} target="_blank">• Deployed application •</a>
+                                <a className="link-success link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href={project.repo} target="_blank">• Github repository •</a>
                             </div>
 
                         </div>
