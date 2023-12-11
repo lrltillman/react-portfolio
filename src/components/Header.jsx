7dc39055b6000afab99770c02bbb1e5ff3672ce5
Leah Tillman <leahtillman@Leahs-MacBook-Air.local>
2023-12-10 21:24:04 -0600

    navbar edits in header component- made it more visible to see which tab was highlighted

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
 
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <a className="navbar-brand" >Leah Tillman</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
